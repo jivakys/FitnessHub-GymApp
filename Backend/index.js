2a0be3dfe101342796a7053f8293396105182de3
Jivak Sute <jivakysute@gmail.com>
2023-08-24 21:24:07 +0530

    login and signup page

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,21 @@
-const express=require('express')
-const {connection}=require("./config/mongo_DB")
-const {userRouter}=require("./routes/userRouter")
-const {UserModel} = require("./models/userModel");
+const express = require("express");
+const { connection } = require("./config/mongo_DB");
+const { userRouter } = require("./routes/userRouter");
 
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.use("/user", userRouter);
 
-const cors = require('cors')
-require('dotenv').config()
-const app=express()
-app.use(cors())
-app.use(express.json())
-
-
-app.use("/user",userRouter);
-
-
-app.listen(process.env.port,async()=>{
-    try {
-        await connection
-        console.log("Connected to the db")
-    } catch (error) {
-        console.log(error)
-    }
-    console.log(`Listening on port ${process.env.port}`)
-})
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to the Database");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Listening on port ${process.env.PORT}`);
+});
--- Backend/models/userModel.js
+++
@@ -1,1 +0,0 @@
-// all user model here
