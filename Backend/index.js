616589b542943a0011974f0241148c9a1f2b4fb2
Jivak Sute <jivakysute@gmail.com>
2023-08-26 17:00:46 +0530

    trainer class page done

--- Backend/index.js
+++ Backend/index.js
@@ -1,53 +1,66 @@
-const express=require('express')
-const {connection}=require("./config/mongo_DB")
-const {userRouter}=require("./routes/userRouter")
-// const {classesRouter}=require("./routes/classesRouter")
+const express = require("express");
+const { connection } = require("./config/mongo_DB");
+const { userRouter } = require("./routes/userRouter");
+const { classesRouter } = require("./routes/classesRouter");
 const { dashboardRouter } = require("./routes/adminRouter");
-const {UserModel} = require("./models/userModel");
-const cors = require('cors')
-require('dotenv').config()
-const app=express()
-
-
-app.use(cors())
-app.use(express.json())
-
-const {passport} = require("./google.outh");
-
+const { UserModel } = require("./models/userModel");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+const { passport } = require("./google.outh");
 
 //...... api point (routes) start here..........//
-app.get("/",(req,res)=>{
-    res.send("Base Endpoint Of API")
-})
-app.use("/user",userRouter);
+app.get("/", (req, res) => {
+  res.send("Base Endpoint Of API");
+});
+app.use("/user", userRouter);
+app.use("/class", classesRouter);
 app.use("/admin", dashboardRouter);
+app.get("/alltrainer", async (req, res) => {
+  try {
+    let trainers = await UserModel.find({ role: "trainer" });
+    res.status(200).send({ message: "User Data Fetched", trainers });
+  } catch (error) {
+    res
+      .status(400)
+      .send({ message: "Something went wrong", error: error.message });
+    console.log(error);
+  }
+});
 //.......api routes end here.........//
 
-
-
-
 //................google Auth..............//
-app.get('/auth/google',passport.authenticate('google', { scope: ['profile','email'] }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' ,session:false}),function(req, res) {
-    res.redirect("www.google.com")
-  });
-
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+  function (req, res) {
+    res.redirect("www.google.com");
+  }
+);
 
 //...............google oath End ...........//
 
-
 //..........server part start..........//
-app.listen(process.env.port,async()=>{
-    try {
-        await connection
-        console.log("Connected to the DB")
-    } catch (error) {
-        console.log(error)
-    }
-    console.log(`Listening on port ${process.env.port}`)
-})
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to the DB");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Listening on port ${process.env.PORT}`);
+});
 
 //..........server part end here.............//
