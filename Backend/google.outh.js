c85b9897eaebb80ebd44c34507e6707fb196c2b3
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-08-26 16:14:48 +0530

    some updates

--- Backend/google.outh.js
+++ Backend/google.outh.js
@@ -1,16 +1,17 @@
 let UserModel = require("./models/userModel");
 const passport = require("passport");
+
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: "761243222092-k07bsrigbpuo1trj04iql0q6mmsar0pr.apps.googleusercontent.com",
+
     clientSecret: "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",
-    callbackURL: "http://localhost:4051/user/auth/google/callback"
+
+    callbackURL: "http://localhost:3456/user/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, cb) {
     
-    // const user = new UserModel({name:profile._json.name,email:profile._json.email,password:"123"});
-    // await user.save()
     return cb(null, "user");
   }
 ));
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const {connection}=require("./config/mongo_DB")
 const {userRouter}=require("./routes/userRouter")
-// const {classesRouter}=require("./routes/classesRouter")
+const {classesRouter}=require("./routes/classesRouter")
+const {ordersRouter}=require("./routes/ordersRouter")
 const { dashboardRouter } = require("./routes/adminRouter");
 const {UserModel} = require("./models/userModel");
 const cors = require('cors')
@@ -21,7 +22,19 @@ app.use(cors({ origin: "*" }));
 app.get("/",(req,res)=>{
     res.send("Base Endpoint Of API")
 })
+
+app.get("/alltrainer", async (req,res)=>{
+    try{
+        let trainers = await UserModel.find({role:"trainer"});
+        res.status(200).send({message:"User Trainer Data Fetched",trainers})
+    }catch(error){
+        res.status(400).send({message:"Something went wrong",error:error.message})
+        console.log(error)
+    }
+})
 app.use("/user",userRouter);
+app.use("/class",classesRouter);
+app.use("/order",ordersRouter);
 app.use("/admin", dashboardRouter);
 //.......api routes end here.........//
 
