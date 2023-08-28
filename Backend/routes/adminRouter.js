f77f9add7e7e3373470365893292cc63470c22ad
Jivak Sute <jivakysute@gmail.com>
2023-08-28 14:48:15 +0530

    some UI changes

--- Backend/routes/adminRouter.js
+++ Backend/routes/adminRouter.js
@@ -1,12 +1,10 @@
-// admin route code here
-
 const dashboardRouter = require("express").Router();
-const {AdminModel} = require("../models/adminModel");
-const {UserModel} = require("../models/userModel")
+const { AdminModel } = require("../models/adminModel");
+const { UserModel } = require("../models/userModel");
 
-dashboardRouter.get("/",(req,res)=>{
-    res.status(200).send({message:"..Admin Page is here.."})
-})
+dashboardRouter.get("/", (req, res) => {
+  res.status(200).send({ message: "..Admin Page is here.." });
+});
 
 dashboardRouter.post("/signin", async (req, res) => {
   let { email, password } = req.body;
@@ -32,25 +30,23 @@ dashboardRouter.post("/signin", async (req, res) => {
   }
 });
 
-dashboardRouter.post('/signup',async(req,res)=>{
+dashboardRouter.post("/signup", async (req, res) => {
   let { name, email, password } = req.body;
   try {
-    let admin =new  AdminModel({name,email,password})
+    let admin = new AdminModel({ name, email, password });
     await admin.save();
-    res.send('Admin Signup Successful')
-  } catch (error) {
-    
-  }
-})
+    res.send("Admin Signup Successful");
+  } catch (error) {}
+});
 
 dashboardRouter.get("/all", async (req, res) => {
-    // res.status(200).send({message:"Details of Users, Trainers, Classes etc.."}) all details we can access from here..//
-    
-    try {
-    let users = await UserModel.find({role:"client"});
-    
+  // res.status(200).send({message:"Details of Users, Trainers, Classes etc.."}) all details we can access from here..//
+
+  try {
+    let users = await UserModel.find({ role: "client" });
+
     // console.log(users)
-    
+
     res.send({
       msg: "..Dashboard Report Done..",
       usersRegistered: users,
@@ -59,8 +55,6 @@ dashboardRouter.get("/all", async (req, res) => {
   } catch (error) {
     console.log({ msg: "Error" });
   }
-})
+});
 
 module.exports = { dashboardRouter };
-
-
