7026793cf1a4fd08f5ec531d834c9fb000d5eb4b
Jivak Sute <jivakysute@gmail.com>
2023-10-13 11:34:34 +0530

    client side validation added

--- Frontend/scripts/signup.js
+++ Frontend/scripts/signup.js
@@ -2,48 +2,127 @@ document.querySelector("#back-to-login").addEventListener("click", () => {
   window.location = "login.html";
 });
 
-const onSignUp = () => {
-  const payload = {
-    name:
-      document.querySelector("#firstName").value +
-      " " +
-      document.querySelector("#lastName").value,
-    email: document.querySelector("#Email").value,
-    password: document.querySelector("#signup-password").value,
-    sex: document.querySelector("#gender").value,
-    role: document.querySelector("#role").value,
-  };
-  console.log(payload);
+function validateFirstName() {
+  const firstNameInput = document.getElementById("firstName");
+  const firstNameValue = firstNameInput.value.trim();
+
+  if (firstNameValue.length < 3) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "First Name must have at least 3 Letters.",
+    });
+    firstNameInput.focus();
+    return false;
+  }
+  return true;
+}
+
+function validateLastName() {
+  const lastNameInput = document.getElementById("lastName");
+  const lastNameValue = lastNameInput.value.trim();
+
+  if (lastNameValue.length < 3) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Last Name must have at least 3 Letters.",
+    });
+    lastNameInput.focus();
+    return false;
+  }
+
+  return true;
+}
+
+function validateEmail() {
+  const emailInput = document.getElementById("Email");
+  const emailValue = emailInput.value.trim();
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!emailPattern.test(emailValue)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please enter a valid Email Address.",
+    });
+    emailInput.focus();
+    return false;
+  }
+
+  return true;
+}
+
+function validatePassword() {
+  const passwordInput = document.getElementById("signup-password");
+  const passwordValue = passwordInput.value;
   if (
-    payload.name == "" ||
-    payload.email == "" ||
-    payload.password == "" ||
-    payload.sex == "" ||
-    payload.role == ""
+    passwordValue.length < 6 ||
+    !/[A-Z]/.test(passwordValue) ||
+    !/[a-z]/.test(passwordValue) ||
+    !/\d/.test(passwordValue)
   ) {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: "Please fill all the details....",
+      text: "Password must be 6 characters with Atlist 1 Uppercase, 1 Lowercase, and 1 Number.",
     });
-    return;
+
+    passwordInput.focus();
+    return false;
   }
+  return true;
+}
 
-  fetch("https://jittery-clam-underclothes.cyclic.app/user/register", {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      Swal.fire("Good job", "User Registered", "success");
+const onSignUp = () => {
+  if (
+    validateFirstName() &&
+    validateLastName() &&
+    validateEmail() &&
+    validatePassword()
+  ) {
+    const payload = {
+      name:
+        document.querySelector("#firstName").value +
+        " " +
+        document.querySelector("#lastName").value,
+      email: document.querySelector("#Email").value,
+      password: document.querySelector("#signup-password").value,
+      sex: document.querySelector("#gender").value,
+      role: document.querySelector("#role").value,
+    };
 
-      setTimeout(() => {
-        window.location = "login.html";
+    if (
+      payload.name == "" ||
+      payload.email == "" ||
+      payload.password == "" ||
+      payload.sex == "" ||
+      payload.role == ""
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Complete All Required Fields.",
       });
+      return;
+    }
+
+    fetch("https://jittery-clam-underclothes.cyclic.app/user/register", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(payload),
     })
-    .catch((err) => console.log(err));
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        Swal.fire("Good job", "Signup successful!", "success");
+
+        setTimeout(() => {
+          window.location = "login.html";
+        }, 1000);
+      })
+      .catch((err) => console.log(err));
+  }
 };
